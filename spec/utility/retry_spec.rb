cc5b46c4c15e1e198cb45adb8698d282656bb2e2
Igor Šarčević <igor@renderedtext.com>
2017-12-16 13:08:46 +0000

    Preserve the exit status

--- spec/utility/retry_spec.rb
+++ spec/utility/retry_spec.rb
@@ -3,28 +3,38 @@ require "open3"
 
 describe "Command Retry Script" do
 
-  it "retries the execution default number of times" do
+  it "retries the execution 3 times" do
     stdout, stderr, status = Open3.capture3("utility/retry false")
 
     msg = [
-      "Execution Failed with exit status 1. Retrying [1/3].",
-      "Execution Failed with exit status 1. Retrying [2/3].",
-      "Execution Failed with exit status 1. Retrying [3/3].",
+      "[1/3] Execution Failed with exit status 1. Retrying.",
+      "[2/3] Execution Failed with exit status 1. Retrying.",
+      "[3/3] Execution Failed with exit status 1. No more retries.",
       ""
     ].join("\n")
 
-    expect(stderr).to eq(msg)
+    expect(stdout).to eq(msg)
+    expect(status.exitstatus).to eq(1)
   end
 
   context "when the command contains complex bash logic" do
     it "can use the retry script if the commands are passed as a string" do
-      output = `utility/retry 3 'for i in {1..10}; { echo $i }; false'`
+      stdout, stderr, status = Open3.capture3("utility/retry --times 3 'for i in {1..2}; { echo $i; }; false'")
 
-      msg = [
+      expect(status.exitstatus).to eq(1)
+
+      expect(stdout).to eq([
+        "1",
+        "2",
+        "[1/3] Execution Failed with exit status 1. Retrying.",
+        "1",
+        "2",
+        "[2/3] Execution Failed with exit status 1. Retrying.",
+        "1",
+        "2",
+        "[3/3] Execution Failed with exit status 1. No more retries.",
         ""
-      ]
-
-      expect(output).to eq(msg.join("\n"))
+      ].join("\n"))
     end
   end
 
